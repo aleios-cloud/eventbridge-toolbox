3a8de4aa1643bdc14b7301526ce6abe4cf092e31
April Bates <aprilb@aleios.com>
2023-08-02 17:06:47 +0100

    feat: changing jsonSchema to interface

--- example-architecture/events/contracts/contract.ts
+++ example-architecture/events/contracts/contract.ts
@@ -1,23 +1,4 @@
-import { JSONSchema } from 'json-schema-to-ts';
-
-export const examplePersonSchema = {
-  $id: "https://example.com/person.schema.json",
-  $schema: "https://json-schema.org/draft/2020-12/schema",
-  title: "Person",
-  type: "object",
-  properties: {
-    firstName: {
-      type: "string",
-      description: "The person's first name.",
-    },
-    lastName: {
-      type: "string",
-      description: "The person's last name.",
-    },
-    age: {
-      description: "Age in years which must be equal to or greater than zero.",
-      type: "integer",
-      minimum: 0,
-    },
-  },
-} as const satisfies JSONSchema;
+export type ExamplePersonContract = {
+  firstName: string;
+  lastName: string;
+};
