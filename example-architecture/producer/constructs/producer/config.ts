bd24a204b28e42064edf4238fa15f33cfb93b621
April Bates <aprilb@aleios.com>
2023-08-02 17:19:55 +0100

    fix: simplifying config

--- example-architecture/producer/constructs/producer/config.ts
+++ example-architecture/producer/constructs/producer/config.ts
@@ -1,38 +1,26 @@
 import { IFunction } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 
-import {
-  NodejsFunction,
-  NodejsFunctionProps,
-} from "aws-cdk-lib/aws-lambda-nodejs";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 
 import { Duration } from "aws-cdk-lib";
 import { Architecture, Runtime, Tracing } from "aws-cdk-lib/aws-lambda";
-
-export class Lambda extends NodejsFunction {
-  constructor(scope: Construct, id: string, props: NodejsFunctionProps) {
-    super(scope, id, {
-      ...props,
-      runtime: Runtime.NODEJS_18_X,
-      architecture: Architecture.ARM_64,
-      tracing: Tracing.ACTIVE,
-      memorySize: 1024,
-      timeout: Duration.seconds(20),
-      retryAttempts: 2,
-      deadLetterQueueEnabled: true,
-    });
-  }
-}
-
+import { getCdkHandlerPath } from "example-architecture/producer/constructs/producer/helpers/getCdkHandlerPath";
 export class Producer extends Construct {
   public producerLambda: IFunction;
-
 
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    this.producerLambda = new Lambda(scope, "producer-lambda", {
-      entry: "./handler",
+    this.producerLambda = new NodejsFunction(this, "producer-lambda", {
+      runtime: Runtime.NODEJS_18_X,
+      architecture: Architecture.ARM_64,
+      tracing: Tracing.ACTIVE,
+      memorySize: 1024,
+      entry: getCdkHandlerPath(__dirname),
+      timeout: Duration.seconds(20),
+      retryAttempts: 2,
+      deadLetterQueueEnabled: true,
     });
   }
 }
