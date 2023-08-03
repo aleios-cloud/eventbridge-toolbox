b08b385bfb2a9e2116a9892406165397a4b74991
April Bates <aprilb@aleios.com>
2023-08-03 15:07:24 +0100

    feat: adding tests

---
+++ example-architecture/producer/constructs/producer/__tests__/importEvent.integration.test-d.ts
@@ -0,0 +1,22 @@
+import * as AWS from "@aws-sdk/client-lambda";
+import { describe, it, assertType } from "vitest";
+import { ExamplePersonContract } from "example-architecture/events";
+const REGION = "eu-west-2";
+
+const lambda = new AWS.Lambda({ region: REGION });
+
+describe("Given a producer lambda that returns a Contract", () => {
+  const params = {
+    FunctionName: "producer-lambda",
+  };
+  describe("When that lambda is invoked", async () => {
+    const invokedLambda = await lambda.invoke(params);
+
+    it("The body returned is of type Contract ", () => {
+      const body = JSON.parse(
+        Buffer.from(invokedLambda.Payload ?? "").toString()
+      );
+      assertType<ExamplePersonContract>(body);
+    });
+  });
+});
