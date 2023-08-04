7ab631308e188dc1bf0d8eee16dab1e3d068bb09
April Bates <aprilb@aleios.com>
2023-08-04 17:56:51 +0100

    fix: replacing dummy data

--- example-architecture/producer/constructs/producer/handler.ts
+++ example-architecture/producer/constructs/producer/handler.ts
@@ -2,8 +2,8 @@ import { ExamplePersonContract } from "example-architecture/events";
 
 export const handler = async (): Promise<ExamplePersonContract> => {
   const contractBody: ExamplePersonContract = {
-    firstName: "April",
-    lastName: "Bates",
+    firstName: "testFirstName",
+    lastName: "testLastName",
   };
   return contractBody;
 };
