5c803ebeccfce39e84f052ea0baac466c24b99ed
April Bates <124585201+april-bates-aleios@users.noreply.github.com>
2023-08-08 11:21:45 +0100

    Hotfix/fixing producer lambda error (#9)
    * feat: making the lambda handler async
    
    * feat: altering handler config
    
    * feat: altering tsconfig

--- example-architecture/producer/functions/producer/handler.ts
+++ example-architecture/producer/functions/producer/handler.ts
@@ -1,6 +1,9 @@
 import { PersonRegisteredContract } from "example-architecture/events/contracts/personRegisteredContract";
 
-export const handler = (): PersonRegisteredContract => {
+// The lambda documentation recommends making the handler async, although there is nothing to await
+// When synchronous this lambda returns null.
+// eslint-disable-next-line @typescript-eslint/require-await
+export const handler = async (): Promise<PersonRegisteredContract> => {
   const contractBody: PersonRegisteredContract = {
     firstName: "testFirstName",
     lastName: "testLastName",
