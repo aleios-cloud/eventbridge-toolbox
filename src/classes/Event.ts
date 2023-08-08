3a34fda44acd94efb819d9d49a29336e35b8d03e
Luke Yianni <lukey@aleios.com>
2023-08-08 11:43:14 +0100

    feat: add IEvent Interface

--- src/classes/Event.ts
+++ src/classes/Event.ts
@@ -1,5 +1,9 @@
-export class Event<Contract> {
-  private data: Contract;
+export interface IEvent<Contract> {
+  readonly data: Contract;
+}
+
+export class Event<Contract> implements IEvent<Contract> {
+  readonly data: Contract;
 
   constructor(contract: Contract) {
     this.data = contract;
