e95d9c72b2ca46fef304e3a9049018552a43354c
April Bates <aprilb@aleios.com>
2023-08-09 16:02:13 +0100

    feat: calling event.publish in producer handler

--- example-architecture/producer/functions/producer/handler.ts
+++ example-architecture/producer/functions/producer/handler.ts
@@ -1,15 +1,18 @@
 import { PersonRegisteredContract } from "example-architecture/events/contracts/personRegisteredContract";
+import { getEnvVariable } from "example-architecture/helpers/getEnvVariable";
 import { Event, IEvent } from "src";
 
-// The lambda documentation recommends making the handler async, although there is nothing to await
-// When synchronous this lambda returns null.
-// eslint-disable-next-line @typescript-eslint/require-await
 export const handler = async (): Promise<IEvent<PersonRegisteredContract>> => {
   const contract: PersonRegisteredContract = {
     firstName: "testFirstName",
     lastName: "testLastName",
   };
+
   const event = new Event(contract);
+
+  const EVENT_BUS_ARN = getEnvVariable("EVENT_BUS_ARN");
+
+  await event.publish(EVENT_BUS_ARN);
 
   return event;
 };
