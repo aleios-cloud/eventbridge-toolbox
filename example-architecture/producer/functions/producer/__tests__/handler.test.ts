bf8ed963d0c5da09476b7a35f624fc888bc2aa3f
April Bates <aprilb@aleios.com>
2023-08-09 17:49:08 +0100

    fix: updating handler test

--- example-architecture/producer/functions/producer/__tests__/handler.test.ts
+++ example-architecture/producer/functions/producer/__tests__/handler.test.ts
@@ -1,16 +1,13 @@
+import { handler } from "example-architecture/producer/functions/producer/handler";
 import { describe, expect, it, vi } from "vitest";
-
-import { handler } from "../handler";
 
 const { mockPublish } = vi.hoisted(() => ({
   mockPublish: vi.fn(),
 }));
 
-const { mockGetData } = vi.hoisted(() => ({
-  mockGetData: vi.fn(),
-}));
-
-mockGetData.mockReturnValue({ mockKey: "mockValue" });
+mockPublish.mockReturnValue({
+  eventId: "mockEventId",
+});
 
 vi.mock("src/classes/Event", async () => {
   const actualModule = await vi.importActual<object>("src/classes/Event");
@@ -19,7 +16,6 @@ vi.mock("src/classes/Event", async () => {
     ...actualModule,
     Event: vi.fn().mockReturnValue({
       publish: mockPublish,
-      getData: mockGetData,
     }),
   };
 });
@@ -28,14 +24,12 @@ process.env.EVENT_BUS_ARN = "mockArn";
 
 describe("Given a producer handler", () => {
   describe("When that handler is run", () => {
-    it("An event publish function is called with an event bus ARN", async () => {
-      await handler();
-      expect(mockPublish).toBeCalledWith("mockArn");
-    });
-
-    it("The handler returns with the output of getData", async () => {
+    it("The handler returns with the output of publish", async () => {
       const result = await handler();
-      expect(result).toStrictEqual({ mockKey: "mockValue" });
+      expect(mockPublish).toBeCalledWith("mockArn");
+      expect(result).toStrictEqual({
+        eventId: "mockEventId",
+      });
     });
   });
 });
