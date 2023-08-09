44ee4b34a780832e9587d34a05c6ddabb1e16bd3
April Bates <aprilb@aleios.com>
2023-08-09 15:49:02 +0100

    feat: creating an event bus and outputting ARN

--- example-architecture/events/lib/eventsStack.ts
+++ example-architecture/events/lib/eventsStack.ts
@@ -1,8 +1,17 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
+import { EventBus } from "aws-cdk-lib/aws-events";
 import { Construct } from "constructs";
 
 export class EventsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    const eventBus = new EventBus(this, "exampleArchitectureEventBus", {
+      eventBusName: "ExampleArchitectureEventBus",
+    });
+
+    new CfnOutput(this, "EventBusARN", {
+      value: eventBus.eventBusArn,
+      exportName: "EventBusARN",
+    });
   }
 }
