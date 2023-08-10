093c4deb8191bef6ae8476d2e3c64e6b909ef290
April Bates <aprilb@aleios.com>
2023-08-10 13:22:03 +0100

    fix: fixing naming inconsistency

--- example-architecture/__tests__/importEvent.integration.test.ts
+++ example-architecture/__tests__/importEvent.integration.test.ts
@@ -7,7 +7,7 @@ const lambda = new Lambda({ region: "eu-west-2" });
 
 describe("Given a producer lambda that returns a Contract", () => {
   const params = {
-    FunctionName: "producer-lambda",
+    FunctionName: "producerLambda",
   };
   describe("When that lambda is invoked", async () => {
     const invokedLambda = await lambda.invoke(params);
