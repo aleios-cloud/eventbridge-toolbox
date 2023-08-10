29af159dd87ad0c2eb691533c19c257eda0a0e31
April Bates <aprilb@aleios.com>
2023-08-10 10:05:37 +0100

    feat: updating integration tests

--- example-architecture/__tests__/importEvent.integration.test.ts
+++ example-architecture/__tests__/importEvent.integration.test.ts
@@ -1,10 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { type PutEventsResponse } from "@aws-sdk/client-eventbridge";
 import { Lambda } from "@aws-sdk/client-lambda";
-import { getEnvVariable } from "example-architecture/helpers/getEnvVariable";
-import { assertType, describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
 
-const lambda = new Lambda({ region: getEnvVariable("AWS_REGION") });
+const lambda = new Lambda({ region: "eu-west-2" });
 
 describe("Given a producer lambda that returns a Contract", () => {
   const params = {
@@ -13,11 +12,19 @@ describe("Given a producer lambda that returns a Contract", () => {
   describe("When that lambda is invoked", async () => {
     const invokedLambda = await lambda.invoke(params);
 
-    it("Event is returned ", () => {
+    it("Response with an eventId is returned without an error", () => {
       const body: PutEventsResponse = JSON.parse(
         Buffer.from(invokedLambda.Payload ?? "").toString(),
       );
-      assertType<PutEventsResponse>(body);
+      expect(body.Entries).toEqual([
+        expect.objectContaining({ EventId: expect.any(String) }),
+      ]);
+      expect(body.Entries).toEqual([
+        expect.not.objectContaining({
+          ErrorCode: expect.any(String),
+          ErrorMessage: expect.any(String),
+        }),
+      ]);
     });
   });
 });
