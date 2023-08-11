b185814d4eb1747794caacf98e9e8567b4bafbfe
RyanT5 <ryanschuller@gmail.com>
2023-08-11 17:09:30 +0100

    fix: only include files that contain contract in name

--- scripts/generate-docs.ts
+++ scripts/generate-docs.ts
@@ -19,64 +19,66 @@ readdir(pathToContracts, (err, files) => {
   if (err) {
     console.error(err);
   } else {
-    files.forEach((file) => {
-      // TODO: remove console.log once script is more stable
-      console.log(file);
-      const pathToFile = path.join(pathToContracts, file);
-      const filenameWithoutExtension = file.split(".")[0];
-      const fileNameWithoutContract = filenameWithoutExtension.endsWith(
-        "Contract",
-      )
-        ? filenameWithoutExtension.replace("Contract", "")
-        : filenameWithoutExtension;
+    files
+      .filter((fileName) => fileName.includes("Contract"))
+      .forEach((file) => {
+        // TODO: remove console.log once script is more stable
+        console.log(file);
+        const pathToFile = path.join(pathToContracts, file);
+        const filenameWithoutExtension = file.split(".")[0];
+        const fileNameWithoutContract = filenameWithoutExtension.endsWith(
+          "Contract",
+        )
+          ? filenameWithoutExtension.replace("Contract", "")
+          : filenameWithoutExtension;
 
-      const eventDocsFilePath = path.join(
-        rootPath,
-        `/docs/${fileNameWithoutContract}`,
-      );
-      mkdirSync(eventDocsFilePath, { recursive: true });
+        const eventDocsFilePath = path.join(
+          rootPath,
+          `/docs/${fileNameWithoutContract}`,
+        );
+        mkdirSync(eventDocsFilePath, { recursive: true });
 
-      const eventMarkdownTemplate = readFileSync(
-        path.join(__dirname, "/doc-template.md"),
-        "utf8",
-      );
-      const markdownWithName = eventMarkdownTemplate.replace(
-        "//name//",
-        fileNameWithoutContract,
-      );
-      // TODO: replace with version from contract path once versioning is implemented
-      const markdownWithVersion = markdownWithName.replace(
-        "//version//",
-        "1.0.0",
-      );
+        const eventMarkdownTemplate = readFileSync(
+          path.join(__dirname, "/doc-template.md"),
+          "utf8",
+        );
+        const markdownWithName = eventMarkdownTemplate.replace(
+          "//name//",
+          fileNameWithoutContract,
+        );
+        // TODO: replace with version from contract path once versioning is implemented
+        const markdownWithVersion = markdownWithName.replace(
+          "//version//",
+          "1.0.0",
+        );
 
-      writeFile(
-        `${eventDocsFilePath}/index.md`,
-        markdownWithVersion,
-        (error) => {
+        writeFile(
+          `${eventDocsFilePath}/index.md`,
+          markdownWithVersion,
+          (error) => {
+            if (error) {
+              // TODO: log error rather than throw once script is more stable
+              throw error;
+            }
+          },
+        );
+
+        const typeToSchemaConfig = {
+          path: pathToFile,
+          tsconfig: path.join(rootPath, "/tsconfig.json"),
+          type: "*",
+        };
+        const schema = tsj
+          .createGenerator(typeToSchemaConfig)
+          .createSchema(typeToSchemaConfig.type);
+        const schemaString = JSON.stringify(schema, null, 2);
+
+        writeFile(`${eventDocsFilePath}/schema.json`, schemaString, (error) => {
           if (error) {
             // TODO: log error rather than throw once script is more stable
             throw error;
           }
-        },
-      );
-
-      const typeToSchemaConfig = {
-        path: pathToFile,
-        tsconfig: path.join(rootPath, "/tsconfig.json"),
-        type: "*",
-      };
-      const schema = tsj
-        .createGenerator(typeToSchemaConfig)
-        .createSchema(typeToSchemaConfig.type);
-      const schemaString = JSON.stringify(schema, null, 2);
-
-      writeFile(`${eventDocsFilePath}/schema.json`, schemaString, (error) => {
-        if (error) {
-          // TODO: log error rather than throw once script is more stable
-          throw error;
-        }
+        });
       });
-    });
   }
 });
