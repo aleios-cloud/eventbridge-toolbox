6180cee2a1ab5191eb130df50ffa632022cb0f11
April Bates <aprilb@aleios.com>
2023-08-11 17:09:00 +0100

    fix: change test descriptions

--- example-architecture/consumer/functions/consumer/__tests__/handler.test.ts
+++ example-architecture/consumer/functions/consumer/__tests__/handler.test.ts
@@ -2,30 +2,30 @@ import { isConsumerContractType } from "example-architecture/consumer/functions/
 import { describe, expect, it } from "vitest";
 
 describe("Given a producer handler", () => {
-  describe("With ConsumerContract type validation function", () => {
-    it("Type validation function returns false if event passed in is undefined", () => {
+  describe("When ConsumerContract type validation function is called", () => {
+    it("Then type validation function returns false if event passed in is undefined", () => {
       expect(isConsumerContractType(undefined)).toStrictEqual(false);
     });
 
-    it("Type validation function returns false if event passed in is not of type object", () => {
+    it("Then type validation function returns false if event passed in is not of type object", () => {
       expect(isConsumerContractType("testString")).toStrictEqual(false);
     });
 
-    it("Type validation function returns false if event passed doesn't contain detail", () => {
+    it("Then type validation function returns false if event passed doesn't contain detail", () => {
       expect(isConsumerContractType({})).toStrictEqual(false);
     });
 
-    it("Type validation function returns false if data within event passed is undefined", () => {
+    it("Then type validation function returns false if data within event passed is undefined", () => {
       expect(isConsumerContractType({ detail: undefined })).toStrictEqual(
         false,
       );
     });
 
-    it("Type validation function returns false if detail passed in is not of type object", () => {
+    it("Then type validation function returns false if detail passed in is not of type object", () => {
       expect(isConsumerContractType({ detail: 100 })).toStrictEqual(false);
     });
 
-    it("Type validation function returns true if detail within event passed is an object", () => {
+    it("Then type validation function returns true if detail within event passed is an object", () => {
       expect(isConsumerContractType({ detail: {} })).toStrictEqual(true);
     });
   });
