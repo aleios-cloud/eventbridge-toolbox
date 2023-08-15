cf131e3b901a3f7fb86c2e494f53821a3f6f0735
April Bates <aprilb@aleios.com>
2023-08-15 17:02:41 +0100

    feat: creating dynamoDB table to confume events

--- example-architecture/consumer/functions/consumer/config.ts
+++ example-architecture/consumer/functions/consumer/config.ts
@@ -1,6 +1,13 @@
 import { Fn } from "aws-cdk-lib";
+import { AttributeType, Table } from "aws-cdk-lib/aws-dynamodb";
 import { EventBus, Rule } from "aws-cdk-lib/aws-events";
 import { LambdaFunction } from "aws-cdk-lib/aws-events-targets";
+import {
+  ManagedPolicy,
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+} from "aws-cdk-lib/aws-iam";
 import { Architecture, IFunction, Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
@@ -12,12 +19,37 @@ export class Consumer extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
+    const lambdaRole = new Role(this, "ConsumerLambdaRole", {
+      assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
+      description: "DynamoDB Consumer Role",
+    });
+
+    lambdaRole.addManagedPolicy(
+      ManagedPolicy.fromAwsManagedPolicyName(
+        "service-role/AWSLambdaBasicExecutionRole",
+      ),
+    );
+
+    const dynamoDBTable = new Table(scope, "consumerTestTable", {
+      tableName: "consumerTestTable",
+      partitionKey: { name: "pk", type: AttributeType.STRING },
+    });
+
+    lambdaRole.addToPolicy(
+      new PolicyStatement({
+        actions: ["dynamodb:PutItem"],
+        resources: [dynamoDBTable.tableArn],
+      }),
+    );
+
     this.consumerLambda = new NodejsFunction(this, "ConsumerLambda", {
       runtime: Runtime.NODEJS_18_X,
       functionName: "consumerLambda",
       architecture: Architecture.ARM_64,
       memorySize: 1024,
       entry: getCdkHandlerPath(__dirname),
+      environment: { TABLE_NAME: dynamoDBTable.tableName },
+      role: lambdaRole,
     });
 
     const eventBusArn: string = Fn.importValue("EventBusARN");
--- example-architecture/consumer/functions/consumer/handler.ts
+++ example-architecture/consumer/functions/consumer/handler.ts
@@ -1,5 +1,43 @@
-/* eslint-disable @typescript-eslint/require-await */
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { getEnvVariable } from "example-architecture/helpers/getEnvVariable";
 
-export const handler = async (event: unknown): Promise<void> => {
+type ConsumerEvent = {
+  version: string;
+  id: string;
+  "detail-type": string;
+  source: string;
+  account: string;
+  time: string;
+  region: string;
+  resources: string;
+  detail: {
+    eventVersion: number;
+    firstName: string;
+    lastName: string;
+  };
+};
+
+const TABLE_NAME = getEnvVariable("TABLE_NAME");
+
+export const handler = async (event: ConsumerEvent): Promise<void> => {
   console.log(event);
+  const dynamoDB = DynamoDBDocumentClient.from(new DynamoDBClient());
+
+  const params = {
+    TableName: TABLE_NAME,
+    Item: {
+      pk: event.id,
+      detailType: event["detail-type"],
+      eventVersion: event.detail.eventVersion,
+      firstName: event.detail.firstName,
+      lastName: event.detail.lastName,
+    },
+  };
+
+  try {
+    await dynamoDB.send(new PutCommand(params));
+  } catch (error) {
+    console.log("DynamoDB didn't work :(", error);
+  }
 };
