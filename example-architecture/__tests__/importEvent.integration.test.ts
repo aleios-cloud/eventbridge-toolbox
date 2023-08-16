7064711b4f336aef9514a4f67d1be8647685a7eb
April Bates <aprilb@aleios.com>
2023-08-16 12:02:00 +0100

    feat: creating e2e test

--- example-architecture/__tests__/importEvent.integration.test.ts
+++ example-architecture/__tests__/importEvent.integration.test.ts
@@ -1,21 +1,80 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { type PutEventsResponse } from "@aws-sdk/client-eventbridge";
+import {
+  DynamoDBClient,
+  GetItemCommand,
+  GetItemCommandOutput,
+} from "@aws-sdk/client-dynamodb";
+import {
+  type PutEventsResponse,
+  type PutEventsResultEntry,
+} from "@aws-sdk/client-eventbridge";
 import { Lambda } from "@aws-sdk/client-lambda";
 import { describe, expect, it } from "vitest";
 
 const lambda = new Lambda({ region: "eu-west-2" });
+const client = new DynamoDBClient();
+
+const getDynamoDBCommand = (eventId: string): GetItemCommand =>
+  new GetItemCommand({
+    TableName: "consumerTestTable",
+    Key: {
+      pk: {
+        S: eventId,
+      },
+    },
+  });
+
+const queryDynamodbReturnsItem = async (
+  eventId: string
+): Promise<GetItemCommandOutput | undefined> => {
+  try {
+    const itemResponse = await client.send(getDynamoDBCommand(eventId));
+
+    return "Item" in itemResponse ? itemResponse : undefined;
+  } catch (error) {
+    console.log("DynamoDB send error :(", error);
+
+    return undefined;
+  }
+};
+
+const pollDynamoDB = async (
+  events: PutEventsResultEntry[] | undefined
+): Promise<object> => {
+  if (
+    events === undefined ||
+    events.length === 0 ||
+    events[0].EventId === undefined
+  )
+    return {};
+
+  const eventId = events[0].EventId;
+  let result = await queryDynamodbReturnsItem(eventId);
+  while (result === undefined) {
+    await wait(500);
+    result = await queryDynamodbReturnsItem(eventId);
+  }
+
+  return result;
+};
+
+const wait = (interval: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, interval);
+  });
+};
 
 describe("Given a producer lambda that returns a Contract", () => {
   const params = {
     FunctionName: "producerLambda",
   };
-  describe("When that lambda is invoked", async () => {
+  describe("When that lambda is invoked and returns a body", async () => {
     const invokedLambda = await lambda.invoke(params);
+    const body: PutEventsResponse = JSON.parse(
+      Buffer.from(invokedLambda.Payload ?? "").toString()
+    );
 
     it("Response with an eventId is returned without an error", () => {
-      const body: PutEventsResponse = JSON.parse(
-        Buffer.from(invokedLambda.Payload ?? "").toString(),
-      );
       expect(body.Entries).toEqual([
         expect.objectContaining({ EventId: expect.any(String) }),
       ]);
@@ -25,6 +84,26 @@ describe("Given a producer lambda that returns a Contract", () => {
           ErrorMessage: expect.any(String),
         }),
       ]);
+    });
+
+    it("Response event is put into dynamoDB table by the consumer", async () => {
+      expect(await pollDynamoDB(body.Entries)).toMatchObject({
+        Item: {
+          pk: expect.objectContaining({
+            S: expect.any(String),
+          }),
+          detailType: {
+            S: "PersonRegisteredContract",
+          },
+          eventVersion: { N: "1" },
+          firstName: {
+            S: "testFirstName",
+          },
+          lastName: {
+            S: "testLastName",
+          },
+        },
+      });
     });
   });
 });
