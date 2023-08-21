8dd2671fb3d106c89aa421b86b86e6ecf9fab6ee
April Bates <aprilb@aleios.com>
2023-08-21 15:01:04 +0100

    feat: creating entry file

--- schema-generator/generate-docs.ts
+++ schema-generator/generate-docs.ts
@@ -3,87 +3,97 @@ import path from "path";
 import { createGenerator } from "ts-json-schema-generator";
 import { fileURLToPath } from "url";
 
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
+export const generateDocs = (): void => {
+  const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
-if (process.argv[2] === "") {
-  throw "Please provide the path to your contracts as the first argument.";
-}
-if (process.argv[3] === "") {
-  throw "Please provide the path to your event catalog events folder as the second argument.";
-}
-
-const pathToContracts = path.join(process.cwd(), process.argv[2]);
-
-readdir(pathToContracts, (err, files) => {
-  if (err) {
-    console.error(err);
-  } else {
-    files
-      .filter((fileName) => fileName.includes("Contract"))
-      .forEach((file) => {
-        console.log(`Found ${file}`);
-
-        const pathToFile = path.join(pathToContracts, file);
-        const filenameWithoutExtension = file.split(".")[0];
-        const fileNameWithoutContract = filenameWithoutExtension.endsWith(
-          "Contract",
-        )
-          ? filenameWithoutExtension.replace("Contract", "")
-          : filenameWithoutExtension;
-
-        const docsFilePath = path.join(process.cwd(), process.argv[3]);
-        if (!existsSync(docsFilePath)) {
-          throw "File path provided for documentation directory is invalid. Directory does not exist.";
-        }
-
-        const eventDocsFilePath = path.join(
-          process.cwd(),
-          `/${process.argv[3]}/${fileNameWithoutContract}`,
-        );
-        mkdirSync(eventDocsFilePath, { recursive: true });
-
-        const eventMarkdownTemplate = readFileSync(
-          path.join(__dirname, "/doc-template.md"),
-          "utf8",
-        );
-        const markdownWithName = eventMarkdownTemplate.replace(
-          "//name//",
-          fileNameWithoutContract,
-        );
-        // TODO: replace with version from contract path once versioning is implemented
-        const markdownWithVersion = markdownWithName.replace(
-          "//version//",
-          "1.0.0",
-        );
-
-        writeFile(
-          `${eventDocsFilePath}/index.md`,
-          markdownWithVersion,
-          (error) => {
-            if (error) {
-              console.log(error);
-            }
-          },
-        );
-
-        const typeToSchemaConfig = {
-          path: pathToFile,
-          tsconfig: path.join(process.cwd(), "/tsconfig.json"),
-          type: "*",
-        };
-        const schema = createGenerator(typeToSchemaConfig).createSchema(
-          typeToSchemaConfig.type,
-        );
-        const jsonSchemaWhiteSpace = 2;
-        const schemaString = JSON.stringify(schema, null, jsonSchemaWhiteSpace);
-
-        writeFile(`${eventDocsFilePath}/schema.json`, schemaString, (error) => {
-          if (error) {
-            console.log(error);
-          }
-        });
-
-        console.log(`Created docs for ${fileNameWithoutContract}`);
-      });
+  if (process.argv[2] === "") {
+    throw "Please provide the path to your contracts as the first argument.";
   }
-});
+  if (process.argv[3] === "") {
+    throw "Please provide the path to your event catalog events folder as the second argument.";
+  }
+
+  const pathToContracts = path.join(process.cwd(), process.argv[2]);
+
+  readdir(pathToContracts, (err, files) => {
+    if (err) {
+      console.error(err);
+    } else {
+      files
+        .filter((fileName) => fileName.includes("Contract"))
+        .forEach((file) => {
+          console.log(`Found ${file}`);
+
+          const pathToFile = path.join(pathToContracts, file);
+          const filenameWithoutExtension = file.split(".")[0];
+          const fileNameWithoutContract = filenameWithoutExtension.endsWith(
+            "Contract"
+          )
+            ? filenameWithoutExtension.replace("Contract", "")
+            : filenameWithoutExtension;
+
+          const docsFilePath = path.join(process.cwd(), process.argv[3]);
+          if (!existsSync(docsFilePath)) {
+            throw "File path provided for documentation directory is invalid. Directory does not exist.";
+          }
+
+          const eventDocsFilePath = path.join(
+            process.cwd(),
+            `/${process.argv[3]}/${fileNameWithoutContract}`
+          );
+          mkdirSync(eventDocsFilePath, { recursive: true });
+
+          const eventMarkdownTemplate = readFileSync(
+            path.join(__dirname, "/doc-template.md"),
+            "utf8"
+          );
+          const markdownWithName = eventMarkdownTemplate.replace(
+            "//name//",
+            fileNameWithoutContract
+          );
+          // TODO: replace with version from contract path once versioning is implemented
+          const markdownWithVersion = markdownWithName.replace(
+            "//version//",
+            "1.0.0"
+          );
+
+          writeFile(
+            `${eventDocsFilePath}/index.md`,
+            markdownWithVersion,
+            (error) => {
+              if (error) {
+                console.log(error);
+              }
+            }
+          );
+
+          const typeToSchemaConfig = {
+            path: pathToFile,
+            tsconfig: path.join(process.cwd(), "/tsconfig.json"),
+            type: "*",
+          };
+          const schema = createGenerator(typeToSchemaConfig).createSchema(
+            typeToSchemaConfig.type
+          );
+          const jsonSchemaWhiteSpace = 2;
+          const schemaString = JSON.stringify(
+            schema,
+            null,
+            jsonSchemaWhiteSpace
+          );
+
+          writeFile(
+            `${eventDocsFilePath}/schema.json`,
+            schemaString,
+            (error) => {
+              if (error) {
+                console.log(error);
+              }
+            }
+          );
+
+          console.log(`Created docs for ${fileNameWithoutContract}`);
+        });
+    }
+  });
+};
---
+++ schema-generator/script.ts
@@ -0,0 +1,9 @@
+//TODO: absolute path here
+import { generateDocs } from "./generate-docs.js";
+
+//TODO: eslint here
+const main = () => {
+  generateDocs();
+
+  console.log("Successfully generated docs:");
+};
